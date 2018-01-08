397a5d119f7001dc993dccbddefe0edf5095a264
Cat Janowitz and Colin Worf <colin@>
2018-01-08 12:01:26 -0800

    add functions to button click array

--- dev/js/interface.js
+++ dev/js/interface.js
@@ -7,7 +7,7 @@ $(function(){
   $('#btn-b').click(function(){
     if (aClicks !=0) {
       pet.whichAction(aClicks);
-
+      return pet.action;
     } else {
       alert('please select a task');
     }
--- dev/js/logic.js
+++ dev/js/logic.js
@@ -3,17 +3,34 @@ export class Tamagotchi {
     this.action;
     this.hungerLvl;
     this.happyLvl;
-
+    this.cleanLvl;
+    this.healthLvl;
   }
 
   whichAction(timesPushed){
     const action = {
-      1 : "Feed",
-      2 : "Play",
-      3 : "Clean",
-      4 : "Heal"
+      1 : this.feedMe(),
+      2 : this.letsPlay(),
+      3 : this.ohNoPoop(),
+      4 : this.ouchieHalp()
     }
     this.action = action[timesPushed]
   }
+
+  feedMe(parameter){
+    this.hungerLvl = 20;
+  });
+
+  letsPlay(parameter){
+    this.happyLvl = 20;
+  });
+
+  ohNoPoop(parameter){
+    this.cleanLvl = 20;
+  });
+
+  ouchieHalp(parameter){
+    this.healthLvl = 20;
+  });
 
 }
